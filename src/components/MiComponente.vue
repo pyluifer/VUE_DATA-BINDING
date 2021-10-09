10708f17c1ee8a8b3498eca640e544a9cf5cebbc
pyluifer <luisf_mach2@yahoo.com>
2021-10-09 17:00:32 -0500

    vue data binding

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{titulo}} </h1>
+    <h2> {{subtitulo}} </h2>
+
+    <input v-model="numero_1" type="number" placeholder="numero 1"/>
+    <br>
+    <input v-model="numero_2" type="number" placeholder="numero 2"/>
+    <br>
+    <button v-on:click="sumar">Sumar</button>
+    <br>
+    <span>resultado: {{resultado}}</span>
+
+  </div>
+</template>
+
+<script>
+export default {
+    // Data contiene las variables globales del componente
+    data: () =>{
+        return {
+            titulo:"Titulo Componente",
+            subtitulo:"Subtitulo Componente",
+            numero_1: null,
+            numero_2: null,
+            resultado: '' 
+        };
+    },
+    methods:{
+        sumar(){
+            console.log(this.numero_1);
+            this.resultado = parseInt(this.numero_1) + parseInt(this.numero_2);
+        }
+    },
+};
+</script>
+
+<style>
+
+</style>
